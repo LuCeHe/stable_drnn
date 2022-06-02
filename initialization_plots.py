c86112e5bd6a488d2cca1bccdee98311d5015f13
LuCeHe <manucelotti@gmail.com>
2022-06-02 20:01:08 +0200

    minor changes

--- initialization_plots.py
+++ initialization_plots.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 from GenericTools.stay_organized.VeryCustomSacred import CustomExperiment
 from alif_sg.neural_models.lsnn import aLSNN
+from stochastic_spiking.visualization_tools.training_tests import get_test_model
 
 FILENAME = os.path.realpath(__file__)
 CDIR = os.path.dirname(FILENAME)
@@ -24,11 +25,12 @@ def config():
     stack = 2
 
     # net
-    # mn_aLSNN_2 mn_aLSNN_2_sig LSNN maLSNN spikingPerformer smallGPT2 aLSNN_noIC spikingLSTM
     net_name = 'maLSNN'
-    n_neurons = 100
+    n_neurons = 128
     batch_size = 128
-    comments = ''
+
+    # LSC dampening:1.
+    comments = 'LSC'
 
     # optimizer properties
 
@@ -41,17 +43,28 @@ def config():
 @ex.capture
 @ex.automain
 def main(comments, seed, n_neurons, stack, initializer, batch_size, _log):
+    exp_dir = os.path.join(*[CDIR, ex.observers[0].basedir])
+    images_dir = os.path.join(*[exp_dir, 'images'])
+
     out_dim = 2
     in_dim = 2
-    lin = tf.keras.layers.Input((100, in_dim))
-    cell = aLSNN(num_neurons=n_neurons)
-    alsnn = tf.keras.layers.RNN(cell, return_sequences=True, name='encoder', stateful=False)(lin)[0]
+    time_steps = 100
+    n_rnns = 1
+    rnns = []
+    for _ in range(n_rnns):
+        cell = aLSNN(num_neurons=n_neurons, config=comments, name='alsnn', initializer=initializer)
+        rnn = tf.keras.layers.RNN(cell, return_sequences=True, stateful=False, name='alsnn')
+        rnns.append(rnn)
 
-    readout = tf.keras.layers.Dense(out_dim)(alsnn)
+    lin = tf.keras.layers.Input((time_steps, in_dim))
+    x = [lin]
+    for rnn in rnns:
+        x = rnn(x[0])
+    readout = tf.keras.layers.Dense(out_dim, name='readout')(x[0])
     model = tf.keras.models.Model(lin, readout)
 
-    tin = tf.convert_to_tensor(np.random.randn(batch_size, 100, in_dim))
-    tout = tf.convert_to_tensor(np.random.randn(batch_size, 100, out_dim))
+    tin = tf.convert_to_tensor(np.random.randn(batch_size, time_steps, in_dim))
+    tout = tf.convert_to_tensor(np.random.randn(batch_size, time_steps, out_dim))
 
     # x = tf.constant(3.0)
     with tf.GradientTape() as g:
@@ -61,18 +74,46 @@ def main(comments, seed, n_neurons, stack, initializer, batch_size, _log):
         loss = tf.keras.losses.MSE(tout, lout)
     dy_dx = g.gradient(loss, tin)
 
-    var = np.var(dy_dx, axis=(0, 2))
-    print('var:  ', var[0], var[-1])
-    mean = np.mean(dy_dx, axis=(0, 2))
-    print('mean: ', mean[0], mean[-1])
-    print(mean.shape, var.shape)
-    Ts = np.arange(1, len(mean)+ 1)
-    fig, axs = plt.subplots(1, 2, gridspec_kw={'wspace': .1, 'hspace': .05}, figsize=(10, 3))
-    axs[0].plot(Ts, mean)
-    axs[1].plot(Ts, var)
+    test_model = get_test_model(model)
+    trt = test_model.predict(tin, batch_size=tin.shape[0])
+    trt = {name: pred for name, pred in zip(test_model.output_names, trt)}
+    print(trt.keys())
+    var_voltage = np.var(trt['alsnn_1'], axis=(0, 2))
+    activity = trt['alsnn']
 
-    for ax in axs:
-        ax.set_yscale('log')
-        # ax.set_xscale('log')
+    var = np.var(dy_dx, axis=(0, 2))
+    print('var g:  ', var[0], var[-1])
+    mean = np.mean(dy_dx, axis=(0, 2))
+    print('mean g: ', mean[0], mean[-1])
+    mean_activity = np.mean(activity, axis=(0, 2))
+    print('mean a: ', mean_activity[0], mean_activity[-1])
+    # print(mean.shape, var.shape)
+    Ts = np.arange(1, len(mean) + 1)
+    fig, axs = plt.subplots(2, 2, gridspec_kw={'wspace': .3, 'hspace': .3}, figsize=(10, 3))
+    axs[0, 0].plot(Ts, mean)
+    axs[0, 0].set_ylabel('gradient\nmean')
+    axs[0, 1].plot(Ts, var)
+    axs[0, 1].set_ylabel('gradient\nvariance')
+
+    print(activity.shape, var_voltage.shape)
+    axs[1, 0].pcolormesh(activity[0].T)
+    axs[1, 0].set_ylabel('neuron\n activity')
+    axs[1, 1].plot(Ts, var_voltage)
+    axs[1, 1].set_ylabel('voltage\nvariance')
+    axs[1, 1].set_xlabel('time')
+
+
+    # for ax in axs:
+    #     ax.set_yscale('log')
+    #     # ax.set_xscale('log')
+
+    # axs[0, 1].set_yscale('log')
+    # axs[1, 1].set_yscale('log')
+    fig.subplots_adjust(top=0.8)
+
+    fig.suptitle(f'Initialization Stats\n{comments}')  # or plt.suptitle('Main title')
+
+    pathplot = os.path.join(CDIR, 'experiments', f'varag_{comments}'.replace(':', '').replace('.', 'p') + '.png')
+    fig.savefig(pathplot, bbox_inches='tight')
 
     plt.show()
--- neural_models/lsnn.py
+++ neural_models/lsnn.py
@@ -23,24 +23,24 @@ class baseLSNN(tf.keras.layers.Layer):
         return dict(list(base_config.items()) + list(self.init_args.items()))
 
     def __init__(self, num_neurons=None, tau=20., beta=1.8, tau_adaptation=20,
-                 dampening_factor=1., ref_period=-1, thr=.01, inh_exc=1.,
+                 dampening=.3, sharpness=1., ref_period=-1, thr=1., inh_exc=1.,
                  n_regular=0, internal_current=0, initializer='orthogonal',
                  config='', v_eq=0,
                  **kwargs):
         super().__init__(**kwargs)
 
         ref_period = str2val(config, 'refp', float, default=ref_period)
+        dampening = str2val(config, 'dampening', float, default=dampening)
+        sharpness = str2val(config, 'sharpness', float, default=sharpness)
         self.init_args = dict(
             num_neurons=num_neurons, tau=tau, tau_adaptation=tau_adaptation,
             ref_period=ref_period, n_regular=n_regular, thr=thr,
-            dampening_factor=dampening_factor, beta=beta, inh_exc=inh_exc,v_eq=v_eq,
+            dampening=dampening, sharpness=sharpness, beta=beta, inh_exc=inh_exc, v_eq=v_eq,
             internal_current=internal_current, initializer=initializer, config=config)
         self.__dict__.update(self.init_args)
 
         self.state_size = (num_neurons, num_neurons, num_neurons, num_neurons)
         self.mask = tf.ones((self.num_neurons, self.num_neurons)) - tf.eye(self.num_neurons)
-        if 'withrecdiag' in self.config:
-            self.mask = tf.ones((self.num_neurons, self.num_neurons))
 
     def decay_v(self):
         return tf.exp(-1 / self.tau)
@@ -49,110 +49,19 @@ class baseLSNN(tf.keras.layers.Layer):
         n_in = input_shape[-1]
         n_rec = self.num_neurons
 
-        stacki = str2val(self.config, 'stacki', int, default=0)
-        decay_v = self.decay_v()
-        dampening = self.dampening_factor
-        sharpness = 1.
-
-        exp = str2val(self.config, 'folder', str, default=0, split_symbol='**')
-        # dpath = os.path.join(exp, 'trained_models', 'dampening_stacki{}.npy'.format(stacki - 1))
-        # dampening_in = 0 if stacki == 0 or not 'deltain' in self.config else np.load(dpath)
-
-        # spath = os.path.join(exp, 'trained_models', 'sharpness_stacki{}.npy'.format(stacki - 1))
-        # sharpness_in = 0 if stacki == 0 or not 'deltain' in self.config else np.load(spath)
-
         input_init = self.initializer
+        recurrent_init = self.initializer
 
-        if '_conditionI_' in self.config:
-            if not 'multreset' in self.config:
-                mean_rec = 1 / (n_rec - 1) * (3 - 2 * decay_v) * self.thr
-            elif 'multreset1' in self.config:
-                mean_rec = 2 / (n_rec - 1) * (1 - decay_v) * self.thr
-            elif 'multreset2' in self.config:
-                mean_rec = 1 / (n_rec - 1) * (2 - decay_v) * self.thr
-            else:
-                mean_rec = 2 / (n_rec - 1) * (1 - decay_v) * self.thr
-            recurrent_init = PluriInitializerI(mean=mean_rec)
-        else:
-            recurrent_init = self.initializer
-            mean_rec = 0
+        self.input_weights = self.add_weight(shape=(n_in, n_rec), initializer=input_init, name='in_weights')
+        self.recurrent_weights = self.add_weight(shape=(n_rec, n_rec), initializer=recurrent_init, name='rec_weights')
 
-        if not 'convWin' in self.config:
-            self.input_weights = self.add_weight(shape=(n_in, n_rec), initializer=input_init, name='input_weights')
-        else:
-            self.input_weights = tf.eye(n_rec)
-
-        if '_conditionII_' in self.config:
-            assert 'taskmean' in self.config and 'taskvar' in self.config
-            if stacki == 0:
-                input_mean = str2val(self.config, 'taskmean', float, default=None)
-                input_var = str2val(self.config, 'taskvar', float, default=None)
-            else:
-                input_mean, input_var = 1 / 2, 1 / 4
-
-            var_rec = (input_var + input_mean ** 2) * tf.math.reduce_variance(self.input_weights) \
-                      * n_in / (n_rec - 1) - mean_rec ** 2 / 2
-
-            recurrent_init = PluriInitializerI(mean=mean_rec, scale=tf.math.sqrt(var_rec))
-
-        if not 'feedforward' in self.config:
-            self.recurrent_weights = self.add_weight(shape=(n_rec, n_rec), initializer=recurrent_init,
-                                                     name='recurrent_weights')
-        else:
-            self.recurrent_weights = 0
-
-        if 'conditionIII' in self.config:
-            od_type = str2val(self.config, 'conditionIII', str, default='c')
-            thr = self.thr if not 'multreset' in self.config else 0
-            dampening = optimize_dampening(self.recurrent_weights, thr=thr, decay=decay_v, w_in=self.input_weights,
-                                           dampening_in=dampening_in, od_type=od_type)
-
-            print(np.mean(dampening))
-            print(np.mean(dampening), np.std(dampening))
-
-        if 'conditionIV' in self.config and not 'optimizetail' in self.config:
-            assert 'exponentialpseudod' in self.config, \
-                "Condition IV has been coded only for exponential SG !"
-
-            os_type = str2val(self.config, 'conditionIV', str, default='b')
-            sharpness = optimize_sharpness(w_rec=self.recurrent_weights, w_in=self.input_weights, decay=decay_v,
-                                           thr=self.thr, dampening=dampening,
-                                           dampening_in=dampening_in,
-                                           sharpness_in=sharpness_in, os_type=os_type, config=self.config)
-
-            print(np.mean(sharpness), np.std(sharpness))
-
-        if 'conditionIV' in self.config and 'optimizetail' in self.config:
-            assert 'ntailpseudod' in self.config, \
-                "Condition IV optimizetail has been coded only for exponential SG !"
-
-            tail = optimize_tail(self.recurrent_weights, self.input_weights, decay_v, self.thr, dampening, sharpness_in,
-                                 dampening_in)
-
-            print(np.mean(tail))
-            print(self.config)
-            self.config = str2val(self.config, 'tailvalue', replace=np.mean(tail))
-            print(self.config)
-
-        print(self.config)
-        # self.inh_exc = tf.ones(self.num_neurons)
         self._beta = tf.concat([tf.zeros(self.n_regular), tf.ones(n_rec - self.n_regular) * self.beta], axis=0)
         self.built = True
 
-        # dpath = os.path.join(exp, 'trained_models', 'dampening_stacki{}.npy'.format(stacki))
-        # np.save(dpath, dampening)
-        # spath = os.path.join(exp, 'trained_models', 'sharpness_stacki{}.npy'.format(stacki))
-        # np.save(spath, sharpness)
-        self.spike_type = SurrogatedStep(config=self.config, dampening=dampening, sharpness=sharpness)
+        self.spike_type = SurrogatedStep(config=self.config, dampening=self.dampening, sharpness=self.sharpness)
 
     def recurrent_transform(self):
         return self.mask * self.recurrent_weights
-
-    def beta_transform(self):
-        return self._beta
-
-    def current_mod(self, i_in):
-        return i_in
 
     def refract(self, z, last_spike_distance):
         new_last_spike_distance = last_spike_distance
@@ -184,7 +93,7 @@ class baseLSNN(tf.keras.layers.Layer):
 
         if not 'noalif' in self.config:
             new_a = decay_a * old_a + (1 - decay_a) * old_z
-            athr = self.thr + new_a * self.beta_transform()
+            athr = self.thr + new_a * self._beta
 
         else:
             # new_a = decay_a * old_a
@@ -201,7 +110,7 @@ class baseLSNN(tf.keras.layers.Layer):
             i_reset = tf.stop_gradient(i_reset)
 
         decay_v = self.decay_v()
-        new_v = decay_v * old_v + self.current_mod(i_in) + i_reset
+        new_v = decay_v * old_v + i_in + i_reset
 
         return new_v
 
@@ -246,26 +155,26 @@ class aLSNN(baseLSNN):
     def build(self, input_shape):
         n_input = input_shape[-1]
 
-        if not 'noalif' in self.config:
-            parameter2trainable = {k: v for k, v in self.__dict__.items()
-                                   if k in ['tau_adaptation', 'thr', 'beta', 'tau']}
-        else:
-            parameter2trainable = {k: v for k, v in self.__dict__.items()
-                                   if k in ['tau', 'thr']}
+        parameter2trainable = {k: v for k, v in self.__dict__.items()
+                               if k in ['tau_adaptation', 'thr', 'beta', 'tau']}
+
         for k, p in parameter2trainable.items():
-            if k in ['tau', 'tau_adaptation', 'thr', 'dampening_factor', 'beta']:
-                initializer = tf.keras.initializers.TruncatedNormal(mean=p, stddev=3 * p / 7)
-                # initializer = tf.keras.initializers.TruncatedNormal(mean=p, stddev=.1 * p / 7)
-            else:
-                initializer = tf.keras.initializers.RandomNormal(stddev=1. / tf.sqrt(n_input))
-
-            if k == 'beta' and 'negbeta' in self.config:
-                initializer = tf.keras.initializers.RandomNormal(stddev=p / tf.sqrt(tf.cast(n_input, tf.float32)))
-
+            initializer = tf.keras.initializers.TruncatedNormal(mean=p, stddev=3 * p / 7)
+            initializer = tf.keras.initializers.Constant(value=p)
             p = self.add_weight(shape=(self.num_neurons,), initializer=initializer, name=k, trainable=True)
             self.__dict__.update({k: p})
+
+        if 'LSC' in self.config:
+            # initializer = tf.keras.initializers.TruncatedNormal(mean=1, stddev=3 * 1 / 7)
+            # initializer = tf.keras.initializers.Constant(value=1)
+            # print(tf.maximum(initializer), tf.minimum(initializer))
+            # decay_a = tf.exp(-1 / self.tau_adaptation)
+            tau = -1 / tf.math.log(1 / 3)
+            # tau = -1 / tf.math.log(.86)
+            print(tau)
+            self.tau = self.add_weight(shape=(self.num_neurons,), initializer=tf.keras.initializers.Constant(value=tau),
+                                       name='tau', trainable=True)
 
         super().build(input_shape)
 
         self._beta = self.beta
-
