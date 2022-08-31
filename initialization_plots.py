cd7ece3bb5e736f6bce4882b960a983893234241
LuCeHe <manucelotti@gmail.com>
2022-08-31 18:20:52 +0200

    minor changes

--- initialization_plots.py
+++ initialization_plots.py
@@ -357,7 +357,7 @@ def double_exp(x, a, b, c, d, e, f, g, h, i, l, m):
         + (c * np.exp(-d * x) + h * 1 / (1 + g * np.abs(x) ** (1 + l))) * np.heaviside(x, .5)  # one of best so far
 
 
-def adapt_sg_shape(data_generator, model, comments):
+def adapt_sg_shape(data_generator, model, comments, test=False):
 
     (tin, tout), = data_generator.__getitem__()
 
@@ -366,12 +366,18 @@ def adapt_sg_shape(data_generator, model, comments):
     trt = {name: pred for name, pred in zip(test_model.output_names, trt)}
 
     activity_names = [k for k in trt.keys() if k.endswith('_3') and k.startswith('encoder')]
+    all_bins = []
+    all_ns = []
     for i, k in enumerate(activity_names):
         cv = trt[k].flatten()
 
         n, bins, patches = plt.hist(cv, 1000, density=True, facecolor='g', alpha=0.5)
+
+        all_bins.append(bins[:-1])
+        all_ns.append(n)
         try:
-            popt, _ = curve_fit(double_exp, bins[:-1], n, p0=(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, .5, .5, .5, .5, .5))
+
+            popt, _ = curve_fit(double_exp, bins[:-1], n, p0=(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, .5, .5, .5, .5, .0))
             print('\n\n            Adapted SG!\n\n')
         except Exception as e:
             print('\n\n            Non Adapted SG error!\n\n')
@@ -385,8 +391,56 @@ def adapt_sg_shape(data_generator, model, comments):
         comments += '_eppseudod'
         for l, v in zip(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'l', 'm'], popt):
             comments += f'_{l}{i}:{v}'
-    return comments
+    if not test:
+        return comments
+    else:
+        return comments, all_bins, all_ns
+
+def test_adapt_sg_shape():
+    import time
+    named_tuple = time.localtime()  # get struct_time
+    time_string = time.strftime("%Y-%m-%d--%H-%M-%S--", named_tuple)
+    random_string = ''.join([str(r) for r in np.random.choice(10, 4)])
+    EXPERIMENT = os.path.join(EXPERIMENTS, time_string + random_string + '_adapt')
+    MODL = os.path.join(EXPERIMENT, 'trained_models')
+
+    for d in [EXPERIMENT, MODL]:
+        os.makedirs(d, exist_ok=True)
+    os.makedirs(EXPERIMENT, exist_ok=True)
+
+
+    task_name = 'heidelberg'
+    timerepeat = 2
+    epochs = 1
+    comments = ''
+    n_neurons = 100
+    stack = 4
+    batch_size = 64
+    loss_name = 'sparse_categorical_crossentropy'
+
+    comments += '_**folder:' + EXPERIMENT + '**_'
+    comments += '_batchsize:' + str(batch_size)
+    gen_train = Task(timerepeat=timerepeat, epochs=epochs, batch_size=batch_size, steps_per_epoch=10,
+                     name=task_name, train_val_test='train', maxlen=100, comments=comments)
+
+    model_args = dict(task_name=task_name, net_name='maLSNN', n_neurons=n_neurons, lr=0.01, stack=stack,
+                      loss_name=loss_name, embedding=None, optimizer_name='SGD', lr_schedule='',
+                      weight_decay=0.1, clipnorm=1., initializer='glorot_normal', comments=comments,
+                      in_len=gen_train.in_len, n_in=gen_train.in_dim, out_len=gen_train.out_len,
+                      n_out=gen_train.out_dim, final_epochs=gen_train.epochs)
+    train_model = build_model(**model_args)
+
+
+    adapt_comments, all_bins, all_ns = adapt_sg_shape(gen_train, train_model, comments, test=True)
+
+    fig, axs = plt.subplots(1, len(all_bins), gridspec_kw={'wspace': .3, 'hspace': .1}, figsize=(10, 5))
+
+    for i, (bin, n) in enumerate(zip(all_bins, all_ns)):
+        axs[i].plot(bin, n, '--', color='r')
+
+
 
 
 if __name__ == '__main__':
-    initialization_tests()
+    # initialization_tests()
+    test_adapt_sg_shape()
