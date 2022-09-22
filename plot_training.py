faa48c84aa6e9e4d7e5f5e6b579b359c55806382
LuCeHe <manucelotti@gmail.com>
2022-09-22 16:15:29 +0200

    minor changes

--- plot_training.py
+++ plot_training.py
@@ -50,11 +50,6 @@ columns_to_remove = [
 ]
 
 
-def shorten_losses(name):
-    shorter_name = name.replace('sparse_', '').replace('categorical_', '').replace('accuracy', 'acc').replace(
-        'crossentropy', 'xe').replace('perplexity', 'ppl')
-    # shorter_name = name
-    return shorter_name
 
 
 if not os.path.exists(CSVPATH):
