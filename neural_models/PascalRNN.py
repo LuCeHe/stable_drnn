f5a613520a513877be73943868ff8009f3630ad7
LuCeHe <manucelotti@gmail.com>
2023-01-07 10:52:17 +0100

    minor changes

--- neural_models/PascalRNN.py
+++ neural_models/PascalRNN.py
@@ -12,18 +12,56 @@ class PascalRNN(tf.keras.layers.Layer):
         self.state_size = (num_neurons,)
 
     def call(self, inputs, states, **kwargs):
-        output = inputs + states
-        new_state = output
+        output = inputs + states[0]
+        new_state = (output,)
         return output, new_state
 
 
-
 if __name__ == '__main__':
+    import numpy as np
+    from scipy import special as sp
+    import matplotlib.pyplot as plt
 
-    L = 100
-    # input_layer= tf.keras.layers.InputLayer(input_shape=[None, 1])
-    layers = [PascalRNN(num_neurons=1) for _ in range(L)]
+    L = 10
+    dl = L
+    T = 100
+    tp = np.arange(T)
+    dt = T - tp
+    n = L + dt - 1
+    k = dl
 
-    model = tf.keras.Sequential(layers)
+    input_layer = tf.keras.layers.Input((None, 1))
+    x = input_layer
+    for _ in range(L):
+        cell = PascalRNN(num_neurons=1)
+        x = tf.keras.layers.RNN(cell, return_sequences=True, return_state=False)(x)
 
-    it = tf.random.uniform(shape=[1, 100])
+    it = tf.ones((1, T, 1))
+    with tf.GradientTape() as g:
+        g.watch(it)
+        model = tf.keras.models.Model(input_layer, outputs=x)
+        output = model(it)
+
+    dy_dx = g.gradient(output, it)
+
+    print(dy_dx)
+    print(output)
+
+    bin = sp.binom(n, k)
+
+    fig, axs = plt.subplots(1, 1, figsize=(4, 3))
+    epsilon = 1e12
+    axs.plot(output[0, :, 0], label='y = PascalRNN(x)')
+    axs.plot(dy_dx[0, :, 0], label='dy/dx')
+    axs.plot(bin + epsilon, label='$\\binom{\,n}{\,k}$')
+    # axs.plot(bin + epsilon, label='${n}\choose{k}$')
+
+    axs.set_xlabel('t')
+    for pos in ['right', 'left', 'bottom', 'top']:
+        axs.spines[pos].set_visible(False)
+
+    plt.legend()
+    plot_filename = f'../experiments/pascal.pdf'
+    fig.savefig(plot_filename, bbox_inches='tight')
+
+    plt.show()
