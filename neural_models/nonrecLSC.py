db23a89e5bd8aaf84f3f241df76ea8a7384a62cc
LuCeHe <manucelotti@gmail.com>
2023-01-12 14:31:03 +0100

    minor changes

--- neural_models/nonrecLSC.py
+++ neural_models/nonrecLSC.py
@@ -90,8 +90,8 @@ def apply_LSC_no_time(build_model, generator, max_dim=1024, n_samples=-1, norm_p
             if epsilon_steps > patience:
                 break
 
-            if True:
-                # try:
+            # if True:
+            try:
                 batch = generator.__getitem__(step)[0]
                 if isinstance(batch, list):
                     batch = [tf.convert_to_tensor(tf.cast(b, tf.float32), dtype=tf.float32) for b in batch]
@@ -130,9 +130,9 @@ def apply_LSC_no_time(build_model, generator, max_dim=1024, n_samples=-1, norm_p
                         if isinstance(nlayerjump, int):
                             pairs[1] = ip + nlayerjump
 
-                        print('\n\n')
-                        print(pairs, len(lnames))
-                        print(lnames[pairs[0]], lnames[pairs[1]])
+                        # print('\n\n')
+                        # print(pairs, len(lnames))
+                        # print(lnames[pairs[0]], lnames[pairs[1]])
                         premodel, intermodel = split_model(model, pairs)
 
                         preinter = premodel(batch)
@@ -255,9 +255,9 @@ def apply_LSC_no_time(build_model, generator, max_dim=1024, n_samples=-1, norm_p
                 show_norm = str(ma_norm.numpy().round(3))
                 show_avw = str(av_weights.numpy().round(3))
 
-            # except Exception as e:
-            #     print(e)
-            #     n_failures += 1
+            except Exception as e:
+                print(e)
+                n_failures += 1
 
             show_failure = str(np.array(n_failures / ((step + 1) + epoch * generator.steps_per_epoch)).round(3))
             pbar.update(1)
