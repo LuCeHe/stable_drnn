7448082a14f294e6841b2f73a18a4fcbc3fa8d92
LuCeHe <manucelotti@gmail.com>
2023-06-03 15:14:52 +0200

    minor changes

--- efficientnet.py
+++ efficientnet.py
@@ -160,8 +160,8 @@ def main(args):
     elif 'lscvar' in args.comments:
         model = build_model(args, input_shape, classes)
         loss = lambda x, y: 0
-        lsclr = str2val(args.comments, 'lsclr', float, default=1.e-4)
-        optimizer = tfa.optimizers.AdaBelief(lr=lsclr, weight_decay=1.e-3)
+        lsclr = str2val(args.comments, 'lsclr', float, default=1.e-2)
+        optimizer = tfa.optimizers.AdaBelief(lr=lsclr)
         # optimizer = tfa.optimizers.Lookahead(adabelief, sync_period=6, slow_step_size=0.5)
         model.compile(optimizer, loss)
         steps_per_epoch = args.steps_per_epoch if args.steps_per_epoch > 0 else None
