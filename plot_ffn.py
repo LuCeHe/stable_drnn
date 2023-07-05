ac54f939f0341725807b23fb2efdf2a76dc965a5
LuCeHe <manucelotti@gmail.com>
2023-07-05 14:10:48 +0200

    minor changes

--- plot_ffn.py
+++ plot_ffn.py
@@ -17,11 +17,11 @@ EXPERIMENTS = r'D:\work\alif_sg\experiments'
 plot_norms_evol = False
 plot_norms_evol_1 = False
 lrs_plot = False
-lrs_plot_2 = False
+lrs_plot_2 = True
 bar_plot = False
 plot_losses = False
 
-missing_exps = True
+missing_exps = False
 remove_incomplete = False
 truely_remove = False
 
