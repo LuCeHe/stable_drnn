9d37e2340ac5085219661f583b4169816e5be265
LuCeHe <manucelotti@gmail.com>
2023-10-06 19:45:02 +0200

    minor changes

--- S5/jax_tests_3.py
+++ S5/jax_tests_3.py
@@ -20,18 +20,16 @@ class simpleLRU(nn.Module):
     d_model: 1
 
     def __call__(self, inputs):
-        print(inputs.shape)
         outputs = jax.lax.associative_scan(jnp.add, inputs)
-        # outputs = jax.vmap(lambda u: jax.lax.associative_scan(jnp.add, u))(inputs)
         return outputs
 
 
 ssm = partial(simpleLRU, d_hidden=features, d_model=features)
-layer = SequenceLayer(
-    ssm=ssm,
-    dropout=.1,
-    d_model=features,
-)
+layer = partial(SequenceLayer, ssm=ssm, dropout=.1, d_model=features)
+
+# batchify it with nn.vmap
+layer = partial(nn.vmap(layer, (0, None), 0))(training=True)
+print(layer)
 
 inps = jnp.arange(0, time_steps)
 inps = jnp.tile(inps, (batch_size, 1))
@@ -45,9 +43,9 @@ dummy_input = jnp.ones((batch_size, time_steps, features))
 init_rng, dropout_rng = jax.random.split(init_rng, num=2)
 
 variables = layer.init({"params": init_rng, "dropout": dropout_rng}, dummy_input)
+
 params = variables["params"]
 f = lambda x: layer.apply({'params': params}, x, rngs={'dropout': dropout_rng})
 outs = f(inps)
 print('outs.shape', outs.shape)
 print(f(inps))
-
