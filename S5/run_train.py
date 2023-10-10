e9abf7ca3c3aaa95b79e745423cd33b4faf0a93a
LuCeHe <manucelotti@gmail.com>
2023-10-10 12:51:44 +0200

    minor changes

--- S5/run_train.py
+++ S5/run_train.py
@@ -128,7 +128,7 @@ if __name__ == "__main__":
 
     # Pretraining Parameters
     parser.add_argument("--ptlr", type=float, default=0.05, help="Learning rate for pretraining")
-    parser.add_argument("--ptbsz", type=int, default=8, help="Pretraining batch size")
+    parser.add_argument("--ptbsz", type=int, default=4, help="Pretraining batch size")
     parser.add_argument("--ptsteps", type=int, default=3000, help="Pretraining steps")
     args = parser.parse_args()
 
