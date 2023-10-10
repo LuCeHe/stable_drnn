68105e94fc9adbafb2a8ccd524bf3944cfdc74b0
LuCeHe <manucelotti@gmail.com>
2023-10-10 14:08:16 +0200

    minor changes

--- S5/jax_pretrain.py
+++ S5/jax_pretrain.py
@@ -64,9 +64,9 @@ def get_radiuses(model, aux_dict):
 def train_step(state, inputs, do_rng, tnt, tnl, wshuff_rng):
     def loss_fn(params):
         # shuffling weights during pretraining
-        for k, v in params.items():
-            for sk, sv in v.items():
-                params[k][sk] = random.shuffle(wshuff_rng, sv)
+        # for k, v in params.items():
+        #     for sk, sv in v.items():
+        #         params[k][sk] = random.shuffle(wshuff_rng, sv)
         rt, rl = state.apply_fn(params, state, do_rng, inputs)
         loss = jnp.mean((rt - tnt) ** 2) + jnp.mean((rl - tnl) ** 2)
         return loss
@@ -102,15 +102,10 @@ def pretrain(
         tx = optax.sgd(learning_rate=ptlr)
     elif optimizer == 'nsgd':
         tx = optax.noisy_sgd(learning_rate=ptlr)
-
+    elif optimizer == 'lion':
+        tx = optax.lion(learning_rate=ptlr)
     else:
         tx = optax.sgd(learning_rate=ptlr)
-
-    if 'doublegd' in ptcomments:
-        tx = optax.chain(
-            tx,
-            optax.adabelief(learning_rate=ptlr),
-        )
 
     if 'nonan' in ptcomments:
         tx = optax.chain(
