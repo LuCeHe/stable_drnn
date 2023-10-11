ad706983dda38083b3d0ea7aabe72fa70e1a82b6
LuCeHe <manucelotti@gmail.com>
2023-10-11 15:21:57 +0200

    minor changes

--- S5/jax_pretrain.py
+++ S5/jax_pretrain.py
@@ -119,7 +119,7 @@ def pretrain(
             optax.zero_nans(),
             optax.clip_by_global_norm(1.0),
             optax.ema(0.8),
-            optax.add_decayed_weights(weight_decay=0.03),
+            optax.add_decayed_weights(weight_decay=0.1),
         )
 
     aux_dict = {}
