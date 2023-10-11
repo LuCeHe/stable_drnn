91eac5bece7f57177010a43bc1d1dd4ef0df00ea
LuCeHe <manucelotti@gmail.com>
2023-10-11 17:38:50 +0200

    minor changes

--- S5/jax_pretrain.py
+++ S5/jax_pretrain.py
@@ -164,20 +164,20 @@ def pretrain(
                     tx2 = optax.adamw(learning_rate=lr)
                     shuff_period = 200
                     optch_period = 500
-                    shuffling = True
+                    shuffling = False
                 else:
                     print('SGD')
                     lr = .1
                     tx2 = optax.sgd(learning_rate=lr, momentum=0.7)
                     shuff_period = 100
                     optch_period = 200
-                    shuffling = True
+                    shuffling = False
 
                 tx2 = optax.chain(
                     tx2,
                     optax.zero_nans(),
                     optax.clip_by_global_norm(1.0),
-                    optax.ema(0.5),
+                    optax.ema(0.3),
                 )
 
                 opt_state = tx2.init(state.params)
