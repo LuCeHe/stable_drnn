ff2d49ba7527633b12c64180f31378fcec0a7dfa
LuCeHe <manucelotti@gmail.com>
2023-11-22 18:33:02 +0100

    minor changes

--- neural_models/recLSC.py
+++ neural_models/recLSC.py
@@ -6,7 +6,7 @@ import tensorflow_addons as tfa
 
 from alif_sg.neural_models.normify import get_norms
 from alif_sg.tools.admin_model_removal import get_pretrained_file
-from lru_unofficial.tf.linear_recurrent_unit import ResLRUCell
+from lru_unofficial.src.lru_unofficial.tf.linear_recurrent_unit import ResLRUCell
 from pyaromatics.keras_tools.esoteric_optimizers.AdamW import AdamW as AdamW2
 
 from pyaromatics.keras_tools.esoteric_layers import AddLossLayer, AddMetricsLayer, SymbolAndPositionEmbedding
