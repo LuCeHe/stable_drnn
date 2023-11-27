464db7185ff3d102a16c88df52d31da88e282e05
LuCeHe <manucelotti@gmail.com>
2023-11-27 23:56:32 +0100

    minor changes

--- S5/bin/run_experiments/run_lra_imdb_lru.sh
+++
@@ -1,6 +0,0 @@
-python run_train.py --activation_fn=full_glu  --bidirectional=False \
-                    --batchnorm=True --bsz=50 \
-                    --d_model=256 --dataset=imdb-classification \
-                    --dt_global=True --epochs=35 --jax_seed=8825365 --lr_factor=0.1 \
-                    --n_layers=6 --p_dropout=0.1 --ssm_lr_base=0.004 \
-                    --ssm_size_base=192 --warmup_end=0 --weight_decay=0.05
--- S5/s5/dataloaders/__init__.py
+++
@@ -1,2 +0,0 @@
-import alif_sg.S5.s5.dataloaders.basic
-from alif_sg.S5.s5.dataloaders.base import SequenceDataset
