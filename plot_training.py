b5a76be890524f2ace33cd2f5afe7c6c6824c285
LuCeHe <manucelotti@gmail.com>
2023-11-29 00:32:39 +0100

    new models I want to try

--- plot_training.py
+++ plot_training.py
@@ -42,7 +42,7 @@ GEXPERIMENTS = [
     # r'D:\work\alif_sg\good_experiments\2023-11-10--decolletc',
 ]
 
-expsid = 'fluctuations'  # effnet als ffnandcnns s5lru mnl fluctuations _decolle
+expsid = 's5lru'  # effnet als ffnandcnns s5lru mnl fluctuations _decolle
 h5path = os.path.join(EXPERIMENTS, f'summary_{expsid}.h5')
 
 lsc_epsilon = 0.02  # 0.02
